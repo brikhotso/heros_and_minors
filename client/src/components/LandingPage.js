d485f2dba9a63b90f78aa57529424d8d4055cd21
brikhotso <brenda.rikhotso@gmail.com>
2024-09-21 08:08:22 +0200

    add logo

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './LandingPage.css'; // Isolated styles for the landing page
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   const [showModal, setShowModal] = useState(false);
